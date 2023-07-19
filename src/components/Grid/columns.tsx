006d7bd05514aa38f4c085b4bc9b3c922bca877d
Esaul Esparza Flores <esaul@MacBook-Pro-de-Esaul.local>
2023-07-19 12:04:27 -0600

    Task #4

--- src/components/Grid/columns.tsx
+++ src/components/Grid/columns.tsx
@@ -14,6 +14,10 @@ export const columnDefs: ColDef[] = [
     { field: "q_au_2", headerName: "Q (au)", sortable: true, filter: 'agNumberColumnFilter', filterParams: numberFilter },
     { field: "period_yr", headerName: "Period (yr)", sortable: true, filter: 'agNumberColumnFilter', filterParams: numberFilter },
     { field: "i_deg", headerName: "Inclination (deg)", sortable: true, filter: 'agNumberColumnFilter', filterParams: numberFilter },
-    { field: "pha", headerName: "Potentially Hazardous", sortable: true, filter: true, filterParams: stringFilter },
+    { field: "pha", headerName: "Potentially Hazardous", sortable: true,
+    cellRenderer: (data:any) => {
+        return data.value === 'Y' ? 'Yes' : data.value === 'N' ? 'No' : '';
+    }, 
+    filter: true, filterParams: stringFilter },
     { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, sortable: true, filter: true, filterParams: stringFilter },
   ];
